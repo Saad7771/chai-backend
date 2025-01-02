b03972d292632dcb4141f717f2e1eb540e11c5cf
Saad Qureshi <saadq7700@gmail.com>
2025-01-02 21:52:53 +0530

    add utils, user and video model

---
+++ src/utils/ApiErrors.js
@@ -0,0 +1,26 @@
+//To standarized error (if error come then it come in this formate)
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+//To standarized response (if response come then it come in this formate)
+
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
