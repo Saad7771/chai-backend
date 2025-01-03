30cee9acc551d8caa8371e60e418e6be7d6579aa
Saad Qureshi <saadq7700@gmail.com>
2025-01-03 20:16:44 +0530

    add file with the help of cloudinary and multer.

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)//user may upload multiple file with same name but the file operation will be on server will be for very tiny ammount of time-->then we uploade it on cloudinary--> and then we will delete it.
+
+    }
+  })
+  
+  export const upload = multer({ 
+    storage,
+ })
