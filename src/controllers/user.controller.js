d37ff516aa01bd4f55e94049c585457b7e2da76b
Saad Qureshi <saadq7700@gmail.com>
2025-01-22 20:42:19 +0530

    use and learn aggrigated pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -337,6 +337,78 @@ const updateUserCoverImage = asyncHandler (async (req, res) => {
 
 })
 
+const getUserChanelProfile = asyncHandler (async(req, res) => {
+    const {username} = req.params
+
+    if (!username?.trim()) {
+        throw new ApiErrors(401,"username is missing")  
+    }
+    
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from:"subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from:"subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscriberTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribeToCount:{
+                    $size: "$subscribeTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribeToCount: 1,
+                isSubscribed: 1,
+                email: 1,
+                coverImage: 1,
+                avatar: 1
+
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiErrors(401,"channel does not exixt");    
+    }
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -346,5 +418,6 @@ export {
     getCurrentUser,
     updateAccountDetail,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChanelProfile
 }
